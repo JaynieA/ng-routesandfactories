6de6f1d0d7da812deebe6e38d83c4ef8a0ce0059
Elliot Thomas <elliotnthomas@gmail.com>
2017-01-10 14:29:27 -0600

    set routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,29 @@
 console.log("in Js");
 
-var myApp = angular.module ('myApp', [])
+var myApp = angular.module ('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: '../views/routes/home.html',
+      controller: 'HomeController'
+    })
+    .when('/charOne', {
+      templateUrl: '../views/routes/charOne.html',
+      controller: 'CharOneController'
+    })
+    .when('/charTwo', {
+      templateUrl: '../views/routes/charTwo.html',
+      controller: 'CharTwoController'
+    })
+    .when('/charThree', {
+      templateUrl: '../views/routes/charThree.html',
+      controller: 'CharThreeController'
+    })
+    .otherwise({
+      redirectTo: 'home'
+    })
+}]);
 
 myApp.factory('characterFactory', function(){
   var factory = {};
