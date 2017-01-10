61f3eeed8375631773c49d083e2144990b9c7e06
Elliot Thomas <elliotnthomas@gmail.com>
2017-01-10 14:57:38 -0600

    add high five ng-click

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -29,14 +29,51 @@ myApp.factory('characterFactory', function(){
   var factory = {};
     factory.total = 0;
     factory.charOneSkill = Math.random();
+    factory.charOneSuccess = 0;
     factory.charTwoSkill = Math.random();
+    factory.charTwoSuccess = 0;
     factory.charThreeSkill = Math.random();
+    factory.charThreeSuccess = 0;
+    factory.getRandomNumber = function(){
+      return Math.random();
+    }
 
 
   return factory
 });//end factory
 
 myApp.controller('HomeController', ['$scope', 'characterFactory',  function($scope, characterFactory){
+  console.log("In NG");
+  console.log('factory -->', characterFactory.charOneSkill);
+  console.log('char total and success', characterFactory.total, characterFactory.charOneSuccess);
+
+}]); //end home controller
+
+myApp.controller('CharOneController', ['$scope', 'characterFactory',  function($scope, characterFactory){
+  console.log("In NG");
+  console.log('factory -->', characterFactory.charOneSkill);
+  $scope.highFive = function (){
+    var randomNumber = characterFactory.getRandomNumber();
+    var charOneSkill = characterFactory.charOneSkill;
+    if (randomNumber >= charOneSkill){
+      characterFactory.total++;
+      characterFactory.charOneSuccess++;
+      console.log('char total and success', characterFactory.total, characterFactory.charOneSuccess);
+    } else {
+      alert ('Unsuccessful!')
+    }
+  }
+
+
+}]); //end home controller
+
+myApp.controller('CharTwoController', ['$scope', 'characterFactory',  function($scope, characterFactory){
+  console.log("In NG");
+  console.log('factory -->', characterFactory.charOneSkill);
+
+}]); //end home controller
+
+myApp.controller('CharThreeController', ['$scope', 'characterFactory',  function($scope, characterFactory){
   console.log("In NG");
   console.log('factory -->', characterFactory.charOneSkill);
 
